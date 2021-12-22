fee36bcf0ae7c97fd3b9a5f44d96bc91a937a5dd
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2021-12-22 13:14:07 +0300

    make-atb-var-som.sh prepares all needed files for the final image.

--- make-atb-var-som.sh
+++ make-atb-var-som.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
-cd uboot-imx
+
+# 1
+cd imx-uboot/
 
 CROSS_COMPILE=aarch64-none-elf- make mrproper
 rm -r ./atb-var-som_build
 
+# 1.1
 #CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ atb_var_som_defconfig
 
+# 1.2.1
 mkdir ./atb-var-som_build
 cp atb_var_som.config atb-var-som_build/.config
 
+# 1.2.2
 #CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ menuconfig
 #cp atb-var-som_build/.config atb_var_som.config
 
 CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ -j8
-cp ~/workspace/imx8/uboot-imx/atb-var-som_build/u-boot-nodtb.bin ~/workspace/imx8/imx-mkimage/iMX8M/
-cp ~/workspace/imx8/uboot-imx/atb-var-som_build/spl/u-boot-spl.bin ~/workspace/imx8/imx-mkimage/iMX8M/
 
-cp ~/workspace/imx8/uboot-imx/atb-var-som_build/arch/arm/dts/atb-var-som.dtb ~/workspace/imx8/imx-mkimage/iMX8M/
-#cp ~/workspace/imx8/uboot-imx/atb-var-som_build/arch/arm/dts/imx8mp-evk.dtb ~/workspace/imx8/imx-mkimage/iMX8M/
+cp ./atb-var-som_build/u-boot-nodtb.bin                 ../imx-mkimage/iMX8M/
+cp ./atb-var-som_build/spl/u-boot-spl.bin               ../imx-mkimage/iMX8M/
+cp ./atb-var-som_build/arch/arm/dts/atb-var-som.dtb     ../imx-mkimage/iMX8M/
+cp ./atb-var-som_build/tools/mkimage                    ../imx-mkimage/iMX8M/mkimage_uboot
+cd ..
 
-cd ../imx-atf/
+# 2
+cd imx-atf/
+
 make distclean
 CROSS_COMPILE=aarch64-none-elf- make PLAT=imx8mp bl31
+
 cp build/imx8mp/release/bl31.bin ../imx-mkimage/iMX8M/
 
+cd ..
 
-cd ../imx-mkimage
+# 3
+cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_1d_dmem.bin ./imx-mkimage/iMX8M/
+cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_1d_imem.bin ./imx-mkimage/iMX8M/
+cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_2d_dmem.bin ./imx-mkimage/iMX8M/
+cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_2d_imem.bin ./imx-mkimage/iMX8M/
+
+# 4
+cd imx-mkimage/
+
 make clean
 make SOC=iMX8MP flash_evk 
+
 sudo dd if=./iMX8M/flash.bin of=/dev/sdc bs=1k seek=32 conv=fsync
 
 cd ..
