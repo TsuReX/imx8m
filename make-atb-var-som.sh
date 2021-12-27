3a1b463976b1e42c0292bcb8e4270cef963559ba
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2021-12-27 15:01:59 +0300

    Linux kernel image and dtb file can be specified and are copied to a flash.

--- make-atb-var-som.sh
+++ make-atb-var-som.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
+uboot_dtb="./imx-uboot/atb-var-som_build/arch/arm/dts/atb-var-som.dtb"
+#linux_dtb="./imx-uboot/atb-var-som_build/arch/arm/dts/atb-var-som.dtb"
+linux_dtb="/home/vasily/workspace/variscite/linux/arch/arm64/boot/dts/freescale/imx8mp-var-som-symphony.dtb"
+#linux="./imx_Image"
+linux="/home/vasily/workspace/variscite/linux/arch/arm64/boot/Image"
+rootfs=
+
 # 1
 cd imx-uboot/
 
 if [[ $1 == "defconfig" ]]
 then
-    CROSS_COMPILE=aarch64-none-elf- make mrproper
-    rm -r ./atb-var-som_build
-    CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ atb_var_som_defconfig
+	CROSS_COMPILE=aarch64-none-elf- make mrproper
+	rm -r ./atb-var-som_build
+	CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ atb_var_som_defconfig
 
 elif [[ $1 == "menuconfig" ]]
 then
-    CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ menuconfig
-#    cp atb-var-som_build/.config atb_var_som.config
+	CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ menuconfig
+#	cp atb-var-som_build/.config atb_var_som.config
 
 else
-        echo "Unknown mode"
-        exit
+	echo "Unknown mode"
+	exit
 fi
 
-
-CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ -j8
+CROSS_COMPILE=aarch64-none-elf- make O=./atb-var-som_build/ -j16
 
 cp ./atb-var-som_build/u-boot-nodtb.bin                 ../imx-mkimage/iMX8M/
 cp ./atb-var-som_build/spl/u-boot-spl.bin               ../imx-mkimage/iMX8M/
@@ -48,8 +54,25 @@ cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_2d_imem.bin ./imx-m
 cd imx-mkimage/
 
 make clean
-make SOC=iMX8MP flash_evk 
+make SOC=iMX8MP BOARD=atb-var-som OUTIMG=atb-var-som.bin flash_img
 
-sudo dd if=./iMX8M/flash.bin of=/dev/sdc bs=1k seek=32 conv=fsync
+sudo dd if=./iMX8M/atb-var-som.bin of=/dev/sdc bs=1k seek=32 conv=fsync
 
 cd ..
+
+#5
+sudo umount -f ./target_flash
+sudo rm -rf ./target_flash
+mkdir ./target_flash
+sudo mount /dev/sdc1 ./target_flash
+
+sudo cp $linux_dtb ./target_flash/dtb
+sudo cp $linux ./target_flash/linux
+if [ ! -z $rootfs ]
+then
+	sudo cp $rootfs ./target_flash
+fi
+sleep 5
+ls -l ./target_flash
+sudo umount ./target_flash
+sudo rm -rf ./target_flash
