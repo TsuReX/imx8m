8f0378d1d6b2c890b23f3c44d49676a3847b9950
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2022-05-04 12:57:34 +0300

    make-atb-board.sh is currently invoked with two parameters: target board and configuration type.

--- make-atb-board.sh
+++ make-atb-board.sh
@@ -6,13 +6,16 @@ linux_dtb="/home/user/building_drive/variscite/linux/build_imx8_var/arch/arm64/b
 linux="/home/user/building_drive/variscite/linux/build_imx8_var/arch/arm64/boot/Image"
 rootfs="./rootfs.cpio.gz"
 
+# 
+# Function configures (if it's needed) and makes uboot binaries
+# and copies it to specified directory.
 make_uboot() {
-	# $1 - configuration type {defconfig|menuconfig|config}
+	# $1 - configuration type {defconfig | menuconfig | config}
 	# $2 - final binaries destination directory
-	# $3 - 
+	# $3 - board {atb-var-som | atb-imx8m-smarc}
 	echo "---> Building uboot"
 	
-	if ! [[ $# -eq 2 ]]
+	if ! [[ $# -eq 3 ]]
 	then
 		return -1
 	fi
@@ -21,8 +24,22 @@ make_uboot() {
 	
 	local binaries_dest_dir=$2
 	
-	#local board="atb_var_som"
-	local board="atb_imx8m_smarc"
+	case $3 in
+		"atb-var-som")
+			local board="atb_var_som"
+			local dtb="atb-var-som.dtb"
+		;;
+		
+		"atb-imx8m-smarc")
+			local board="atb_imx8m_smarc"
+			local dtb="atb-imx8m-smarc.dtb"
+		;;
+		
+		*)
+			return -2
+		;;
+	esac 
+	
 	
 	echo "---> Board: "${board}
 	
@@ -54,45 +71,62 @@ make_uboot() {
 	
 	if ! [[ $? -eq 0 ]]
 	then
-		return -2
+		return -3
 	fi
 		
-	cp "$buildig_directory/u-boot-nodtb.bin"                $binaries_dest_dir
-	cp "$buildig_directory/spl/u-boot-spl.bin"              $binaries_dest_dir
-	cp "$buildig_directory/arch/arm/dts/${board}.dtb"     	$binaries_dest_dir
-	cp "$buildig_directory/tools/mkimage"                  	$binaries_dest_dir/mkimage_uboot
+	cp "$buildig_directory/u-boot-nodtb.bin"		$binaries_dest_dir
+	cp "$buildig_directory/spl/u-boot-spl.bin"		$binaries_dest_dir
+	cp "$buildig_directory/arch/arm/dts/${dtb}"		$binaries_dest_dir
+	cp "$buildig_directory/tools/mkimage"			$binaries_dest_dir/mkimage_uboot
 	cd ..
 	
 	return 0
 }
 
+#
+# Function makes ARM Trusted Firmware binaries for specified platform
+# and cpies it to specified directory
 make_atf() {
+	# $1 - final binaries destination directory
+	# $2 - board {atb-var-som | atb-imx8m-smarc}
 	echo "---> Building ATF"
 	
-	if ! [[ $# -eq 1 ]]
+	if ! [[ $# -eq 2 ]]
 	then
 		return -1
 	fi
 	
+	case $2 in
+		"atb-var-som")
+			local target_soc="imx8mp"
+		;;
+		
+		"atb-imx8m-smarc")
+			local target_soc="imx8mq"
+		;;
+		
+		*)
+			return -2
+		;;
+	esac
+	
 	local binaries_dest_dir=$1
 	local target_binary="bl31"
-	#local target_soc="imx8mp"
-	local target_soc="imx8mq"
 	local debug_enable=0
 	
 	cd imx-atf/
 	
 	make distclean
-	CROSS_COMPILE=aarch64-none-elf- make $target_binary PLAT=$target_soc DEBUG=$debug_enable
+	CROSS_COMPILE=aarch64-none-elf- make $target_binary PLAT=$target_soc DEBUG=$debug_enable -j16
 	if ! [[ $? -eq 0 ]]
 	then
-		return -2
+		return -3
 	fi
 	
 	cp "build/$target_soc/release/$target_binary.bin" $binaries_dest_dir
 	if ! [[ $? -eq 0 ]]
 	then
-		return -3
+		return -4
 	fi
 
 	cd ..
@@ -100,9 +134,12 @@ make_atf() {
 	return 0
 }
 
+#
+# Function copies needed for soc booting biraies.
+#
+# TODO: Make pathes to be obtained from arguments
 make_aux_binaries() {
 	echo "---> Preparing auxilary binaries"
-	
 	cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_1d_dmem.bin 	./imx-mkimage/iMX8M/
 	cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_1d_imem.bin 	./imx-mkimage/iMX8M/
 	cp ./firmware-imx-8.9/firmware/ddr/synopsys/lpddr4_pmu_train_2d_dmem.bin 	./imx-mkimage/iMX8M/
@@ -112,7 +149,13 @@ make_aux_binaries() {
 	return 0
 }
 
+#
+# Function formates specified block device
+# and creates partiotions with file systems where OS's files will be copied to.
+#
+# TODO: Give name for each one of stages
 prepare_storage() {
+	# $1 - path to a block device that will be formated and prepared for file systems
 	echo "---> Preparing storage"
 	
 	if ! [[ $# -eq 1 ]]
@@ -145,34 +188,62 @@ prepare_storage() {
 	return 0
 }
 
+#
+# Functions places prepared bootloader binary to specified block device. 
+#
 make_image() {
+	# $1 - board {atb-var-som | atb-imx8m-smarc}
 	echo "---> Preparing image"	
+	
+	if ! [[ $# -eq 1 ]]
+	then
+		return -1
+	fi
+
+	local board=$1
+	case $1 in
+		"atb-var-som")
+			local soc="iMX8MP"
+			local seek=32
+		;;
+		
+		"atb-imx8m-smarc")
+			local soc="iMX8MQ"
+			local seek=33
+		;;
+		
+		*)
+			return -2
+		;;
+	esac
 
 	cd imx-mkimage
 
 	make clean
-	#make SOC=iMX8MP BOARD=atb-var-som OUTIMG=usd_flash.bin flash_img
-	make SOC=iMX8MQ BOARD=atb-im8m-smarc OUTIMG=usd_flash.bin flash_img
+
+	make SOC=${soc} BOARD=${board} OUTIMG=usd_flash.bin flash_img
+	sudo dd if=./iMX8M/usd_flash.bin of=/dev/sdc bs=1k seek=${seek} conv=fsync
+
 	sha256sum ./iMX8M/usd_flash.bin
-	
-	#sudo dd if=./iMX8M/usd_flash.bin of=/dev/sdc bs=1k seek=32 conv=fsync
-	sudo dd if=./iMX8M/usd_flash.bin of=/dev/sdc bs=1k seek=33 conv=fsync
 	
 	cd ..
 	
 	return 0
 }
 
+#
+# Function copies to target storage device binaries needed for OS booting: linux kernel, dtb, root file system image.
+#
+# TODO: Make paremeters to be obtained from arguments
 prepare_os_images_strorage() {
 	echo "---> Preparing kernel, dtb and rootfs files on the bootable storage"
-
+	
 	sudo rm -rf ./target_flash_p1
 	mkdir ./target_flash_p1
 	sudo mount /dev/sdc1 ./target_flash_p1
 	sudo rm -rf ./target_flash_p1/*
 	
 	find ./rootfs | cpio -H newc -o | gzip -9 > _rootfs.cpio.gz ; ./imx-mkimage/iMX8M/mkimage_uboot -A arm -T ramdisk -C gzip -d _rootfs.cpio.gz rootfs.cpio.gz; rm _rootfs.cpio.gz
-	
 	sudo cp $linux_dtb ./target_flash_p1/dtb
 	sha256sum $linux_dtb ./target_flash_p1/dtb
 	
@@ -194,6 +265,10 @@ prepare_os_images_strorage() {
 	
 }
 
+#
+# Function copies to target storage device 
+#
+# TODO: Make paremeters to be obtained from arguments
 prepare_rootfs() {
 	echo "---> Preparing root fs on the bootable storage"
 
@@ -209,24 +284,35 @@ prepare_rootfs() {
 
 }
 
+# The script entry point is here.
+# The script's arguments are:
+# $1 - configuration type {defconfig | menuconfig | config}
+# $2 - board {atb-var-som | atb-imx8m-smarc}
+
+if ! [[ $# -eq 2 ]]
+then
+	echo FAILED
+	exit -1
+fi
+
 # 1
-make_uboot $1 "../imx-mkimage/iMX8M/"
+make_uboot $1 "../imx-mkimage/iMX8M/" $2
 ret_val=$?
 if ! [[ $ret_val -eq 0 ]]
 then
 	echo FAILED
-	exit -1
+	exit -11
 else
 	echo SUCCESS
 fi
 
 # 2
-make_atf "../imx-mkimage/iMX8M/"
+make_atf "../imx-mkimage/iMX8M/" $2
 ret_val=$?
 if ! [[ $ret_val -eq 0 ]]
 then
 	echo FAILED
-	exit -2
+	exit -12
 else
 	echo SUCCESS
 fi
@@ -237,7 +323,7 @@ ret_val=$?
 if ! [[ $ret_val -eq 0 ]]
 then
 	echo FAILED
-	exit -4
+	exit -14
 else
 	echo SUCCESS
 fi
@@ -248,29 +334,30 @@ ret_val=$?
 if ! [[ $ret_val -eq 0 ]]
 then
 	echo FAILED
-	exit -5
+	exit -15
 else
 	echo SUCCESS
 fi
 
 # 4
-make_image
+make_image $2
 ret_val=$?
 if ! [[ $ret_val -eq 0 ]]
 then
 	echo FAILED
-	exit -6
+	exit -16
 else
 	echo SUCCESS
 fi
 exit
+
 #5
 prepare_os_images_strorage
 ret_val=$?
 if ! [[ $ret_val -eq 0 ]]
 then
 	echo FAILED
-	exit -6
+	exit -16
 else
 	echo SUCCESS
 fi
@@ -281,7 +368,7 @@ ret_val=$?
 if ! [[ $ret_val -eq 0 ]]
 then
 	echo FAILED
-	exit -7
+	exit -17
 else
 	echo SUCCESS
 fi
